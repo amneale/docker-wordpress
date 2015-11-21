ddc94d67df3bea217d31c789276fcb54948fd5b5
Adam Neale <adam@adam-neale.co.uk>
2015-11-21 12:31:03 +0000

    detect forward proxy ssl

--- wp-config.php
+++ wp-config.php
@@ -1,23 +1,28 @@
 <?php
 
-$dsn=$_ENV['DATABASE_URL'];
+$dsn = $_ENV['DATABASE_URL'];
 preg_match( '|([a-z0-9]+)://([^:]*)(:(.*))?@([A-Za-z0-9\.-]*)(:([0-9]*))?(/([0-9a-zA-Z_/\.-]*))|', $dsn, $matches);
 
 define('DB_HOST', $matches[5]);
 define('DB_USER', $matches[2]);
 define('DB_PASSWORD', $matches[4]);
-define('DB_NAME',$matches[9]);
+define('DB_NAME', $matches[9]);
 
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', ''     );
+define('DB_CHARSET', 'utf8' );
+define('DB_COLLATE', '');
 
 define('FS_METHOD', 'direct');
 
-$table_prefix  = 'wp_';
+$table_prefix = 'wp_';
 
 define('WP_DEBUG', false);
 
-if ( !defined('ABSPATH') )
-  define('ABSPATH', dirname(__FILE__) . '/');
+if (!defined('ABSPATH')) {
+    define('ABSPATH', dirname(__FILE__) . '/');
+}
+
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+    $_SERVER['HTTPS'] = 'on';
+};
 
 require_once(ABSPATH . 'wp-settings.php');
